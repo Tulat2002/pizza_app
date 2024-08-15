032958b7c9be95f7c75ae7887873883ee1be651e
Tulat2002 <126972147+Tulat2002@users.noreply.github.com>
2024-08-16 00:22:32 +0700

    done Home screen and Detail item screen

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:pizza_app/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 import 'package:pizza_app/screens/auth/view/welcome_screen.dart';
-import 'package:pizza_app/screens/home/views/home_screen.dart';
+import 'package:pizza_app/screens/home/blocs/get_pizza_bloc/get_pizza_bloc.dart';
+import 'package:pizza_repository/pizza_repository.dart';
+
+import 'screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -16,12 +20,23 @@ class MyAppView extends StatelessWidget {
         home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
           builder: ((context, state) {
             if (state.status == AuthenticationStatus.authenticated) {
-              return const HomeScreen();
+              return MultiBlocProvider(
+                providers: [
+                  BlocProvider(
+                    create: (context) => SignInBloc(context.read<AuthenticationBloc>().userRepository),
+                  ),
+                  BlocProvider(
+                    create: (context) => GetPizzaBloc(
+                        FirebasePizzaRepo()
+                    )..add(GetPizza()),
+                  ),
+                ],
+                child: const HomeScreen(),
+              );
             } else {
               return const WelcomeScreen();
             }
           }),
-        )
-    );
+        ));
   }
 }
